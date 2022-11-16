c55f43d4efa830c5c3963e5a009f7d0e3ac429d4
happyHHyu <117804376+happyHHyu@users.noreply.github.com>
2022-11-16 14:36:15 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,9 +7,14 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.AB, function () {
     for (let index = 0; index < set_time; index++) {
+        start_time = input.runningTime()
         set_time += -1
         basic.showNumber(set_time)
+        end_time = input.runningTime()
+        basic.pause(1000 - (end_time - start_time))
     }
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+    basic.showString("TIME'S UP")
 })
 input.onButtonPressed(Button.B, function () {
     set_time += 1
@@ -18,6 +23,8 @@ input.onButtonPressed(Button.B, function () {
     }
     basic.showNumber(set_time)
 })
+let end_time = 0
+let start_time = 0
 let set_time = 0
 set_time = 5
 basic.showNumber(set_time)
