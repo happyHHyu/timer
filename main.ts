76abe7a75c099f1b9b44723a3bec4ae2d74e6e4c
happyHHyu <117804376+happyHHyu@users.noreply.github.com>
2022-11-16 14:12:31 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,6 +5,9 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     set_time += 1
     basic.showNumber(set_time)
+    if (set_time > 9) {
+        set_time = 1
+    }
 })
 let set_time = 0
 set_time = 5
