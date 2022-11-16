9d8df8d23f288e3154a1b51a24a517e15d29491b
happyHHyu <117804376+happyHHyu@users.noreply.github.com>
2022-11-16 14:24:56 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,6 +5,12 @@ input.onButtonPressed(Button.A, function () {
     }
     basic.showNumber(set_time)
 })
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < set_time; index++) {
+        set_time += -1
+        basic.showNumber(set_time)
+    }
+})
 input.onButtonPressed(Button.B, function () {
     set_time += 1
     if (set_time > 9) {
@@ -14,6 +20,7 @@ input.onButtonPressed(Button.B, function () {
 })
 let set_time = 0
 set_time = 5
+basic.showNumber(set_time)
 basic.forever(function () {
 	
 })
